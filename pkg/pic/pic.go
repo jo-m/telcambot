6825089f45c0dae27ce3deddc27d11ff257e6d99
Jonathan Müller <jo-m@outlook.com>
2022-11-27 14:50:16 +0100

    factor out

--- pkg/pic/pic.go
+++ pkg/pic/pic.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"image"
-	"image/color"
 	"image/jpeg"
 	"time"
 
@@ -42,40 +41,6 @@ func chooseFormat(cam *webcam.Webcam, preferred string) (webcam.PixelFormat, str
 	}
 
 	return format, formatStr, &frameSizes[0], nil
-}
-
-type YCbCr struct {
-	rect image.Rectangle
-	buf  []byte
-}
-
-// compile time interface check
-var _ image.Image = (*YCbCr)(nil)
-
-func (i *YCbCr) ColorModel() color.Model {
-	return color.YCbCrModel
-}
-func (i *YCbCr) Bounds() image.Rectangle {
-	return i.rect
-}
-func (i *YCbCr) At(x, y int) color.Color {
-	if !(image.Point{x, y}.In(i.rect)) {
-		return color.YCbCr{}
-	}
-	pixIx := (y*i.rect.Max.X + x)
-	// two pixels = four bytes
-	cellIx := (pixIx / 2) * 4
-
-	Y0, Cb, Y1, Cr := i.buf[cellIx+0], i.buf[cellIx+1], i.buf[cellIx+2], i.buf[cellIx+3]
-
-	Y := Y0
-	if pixIx%2 == 1 {
-		Y = Y1
-	}
-
-	return color.YCbCr{
-		Y, Cb, Cr,
-	}
 }
 
 func convertFrame(frame []byte, formatStr string, w, h int) (image.Image, error) {
